8e0fbf63a886cf1e669617f8a3c90fb4cdda8464
Habib <hk6632521@gmail.com>
2023-06-26 00:42:39 +0530

    edit funtion

--- index.js
+++ index.js
@@ -1,73 +1,187 @@
-var selectedRow = null;
-function onFormSubmit(){
-    event.preventDefault();
-    var formData = readFormData();
-    if(selectedRow === null){
-        insertNewRecord(formData);
-        
-    }else{
+var EmployeeIDs = [];
+var ID = document.getElementById('Emp_id').value;
+var name = document.getElementById('EmpName').value;
+var age = document.getElementById('EmpAge').value;
+var gender = document.getElementById('Emp_gender').value;
 
+var formData;
+
+var selectedRow;
+
+function VerifyId(formData){
+    let isValid = true
+    var i=0;
+    for(i=0;i<=EmployeeIDs.length-1;i++){
+    if(ID==EmployeeIDs[i].Emp_id){
+        isValid=false;
+        console.log('ds')
     }
-    formData.innerHTML = '';
-}
+    }
+    return isValid
+    
+};
 
-// allow only alphabets
-const alphabets = document.getElementById('EmpName');
-alphabets.addEventListener('input',function(event){
-    const inputValue = event.target.value;
-    const sanitizedValue = inputValue.replace(/[^A-Za-z]/g, '');
-    event.target.value= sanitizedValue;
-});
+function insertNewRecord(formData){
+    if(VerifyId(formData)){
+    EmployeeIDs.push(formData);
+    console.log(EmployeeIDs);
+    var table = document.getElementById('emp_list').getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    newRow.innerHTML = `<td>${formData.Emp_id}</td>
+    <td>${formData.EmpName}</td>
+    <td>${formData.EmpAge}</td>
+    <td>${formData.Emp_gender}</td>
+    <td>
+    <button class="editButton" onclick='onEdit(${EmployeeIDs.length - 1})'>Edit</button>
+    <button class="delete_btn">Delete</button>
+    </td>`;
+    tbody.appendChild(newRow);
+    }else{
+        console.log('fds')
+    }
+};
 
 
-function readFormData(){
+
+function validateForm(){
+    event.preventDefault();
     var ID = document.getElementById('Emp_id').value;
     var name = document.getElementById('EmpName').value;
     var age = document.getElementById('EmpAge').value;
     var gender = document.getElementById('Emp_gender').value;
 
+
+    var idError = document.getElementById('idError');
+    var nameError = document.getElementById('nameError');
+    var ageError = document.getElementById('ageError');
+    var selectError = document.getElementById('selectError');
+
+    idError.textContent  = '';
+    nameError.textContent = '';
+    ageError.textContent = '';
+    selectError.textContent = '';   
+    document.getElementById('Emp_id').value = '';
+    document.getElementById('EmpName').value = '';
+    document.getElementById('EmpAge').value = '';
+    document.getElementById('Emp_gender').value= '';
+
+
+    var isValid = true;
+    
     var formData = {
         Emp_id : ID,
         EmpName : name,
         EmpAge : age,
         Emp_gender : gender
     };
-    // formData['Emp_id'] = document.getElementById('Emp_id').value;
-    // formData['EmpName'] = document.getElementById('EmpName').value;
-    // formData['EmpAge'] = document.getElementById('EmpAge').value;
-    // formData['Emp_gender'] = document.getElementById('Emp_gender').value;
-    // formData['Emp_id'] = ID;
-    // formData['EmpName'] = name;
-    // formData['EmpAge'] = age ;
-    // formData['Emp_gender'] = gender;
-    return formData;
-}
-
-function insertNewRecord(data){
-    var table = document.getElementById('emp_list').getElementsByTagName('tbody')[0];
-    var newRow = table.insertRow(table.length);
-    var cell1 = newRow.insertCell(0);
-        cell1.innerHTML = data.Emp_id;
-    var cell2 = newRow.insertCell(1);
-        cell2.innerHTML = data.EmpName;
-    var cell3 = newRow.insertCell(2);
-        cell3.innerHTML = data.EmpAge;
-    var cell4 = newRow.insertCell(3);
-        cell4.innerHTML = data.Emp_gender;
-    var cell5 = newRow.insertCell(4);
-        cell5.innerHTML = `<button  onclick='onEdit()'>Edit</button> <button class='delete_btn'>Delete</button>`;
-}
-
-// function onEdit(td){
-//     selectedRow = td.parentElement.parentElement;
-//     document.getElementById('Ep_id').value = selectedRow.cells[0].innerHTML;
-//     document.getElementById('EmpName').value = selectedRow.cells[1].innerHTML;
-//     document.getElementById('EmpAge').value = selectedRow.cells[2].innerHTML;
-//     document.getElementById('Emp_gender').value = selectedRow.cells[3].innerHTML;
 
 
-// }
-
-function onEdit(){
+    if(formData.Emp_id.trim() === ""){
+        idError.textContent = "Employee ID is required"
+        isValid = false;
+    }else if(EmployeeIDs.some((emp)=>emp.Emp_id === formData.Emp_id)){
+        idError.textContent = 'Id already exist'
+        isValid =false;
+    }
     
+    if(formData.EmpName.trim() === ""){
+        nameError.textContent = 'Name is required'
+        isValid = false;
+    }
+    if(formData.EmpAge.trim()===""){
+        ageError.textContent  = 'This field is Required'
+        isValid = false;
+    }
+    if(formData.Emp_gender === ""){
+        selectError.textContent = "select field"
+        isValid = false;
+    }
+    if(isValid == true){
+ 
+        insertNewRecord(formData);
+    }
+}
+
+
+// allow only Alphabets as Name
+function restrictToAlphabets(inputElement) {
+    inputElement.addEventListener('input', function(event) {
+        const inputValue = event.target.value;
+        const sanitizedValue = inputValue.replace(/[^A-Za-z]/g, '');
+        event.target.value = sanitizedValue;
+    });
+};
+
+  // Assign the function to two input fields
+const EmpName= document.getElementById('EmpName');
+restrictToAlphabets(EmpName);
+
+const editEmpName = document.getElementById('editEmpName');
+restrictToAlphabets(editEmpName);
+
+
+// only eligable Age group
+function ageValidate(inputElement) {
+    inputElement.addEventListener('input', function(event) {
+    const age = parseInt(event.target.value);
+    
+    if (isNaN(age) || age < 18 ) {
+        inputElement.setCustomValidity('Invalid age. Please enter an age between 18 and 59.');
+
+    } else if(age >= 60){
+        inputElement.setCustomValidity('EMployee too old')
+
+    }else{
+        inputElement.setCustomValidity('');
+    }
+    });
+    };
+    
+    // Assign the function to two age input fields
+    const EmpAge = document.getElementById('EmpAge');
+    ageValidate(EmpAge);
+    
+    const editEmpAge = document.getElementById('editEmpAge');
+    ageValidate(editEmpAge);
+
+
+    function onEdit(index) {
+        const employee = EmployeeIDs[index];
+        document.getElementById('editEmpId').value = employee.Emp_id;
+        document.getElementById('editEmpName').value = employee.EmpName;
+        document.getElementById('editEmpAge').value = employee.EmpAge;
+        document.getElementById('editEmpGender').value = employee.Emp_gender;
+        selectedRow = index;
+        document.getElementById('editPopup').style.display = 'block';
+    }
+
+    function saveChanges() {
+        
+        var editedEmpId = document.getElementById('editEmpId').value;
+        var editedEmpName = document.getElementById('editEmpName').value;
+        var editedEmpAge = document.getElementById('editEmpAge').value;
+        var editedEmpGender = document.getElementById('editEmpGender').value;
+      
+        // Update the EmployeeIDs array with the edited values
+        EmployeeIDs[selectedRow].Emp_id = editedEmpId;
+        EmployeeIDs[selectedRow].EmpName = editedEmpName;
+        EmployeeIDs[selectedRow].EmpAge = editedEmpAge;
+        EmployeeIDs[selectedRow].Emp_gender = editedEmpGender;
+      
+        // Update the table row with the edited values
+        var table = document.getElementById('emp_list');
+        var row = table.rows[selectedRow + 2];
+        row.cells[0].innerHTML = editedEmpId;
+        row.cells[1].innerHTML = editedEmpName;
+        row.cells[2].innerHTML = editedEmpAge;
+        row.cells[3].innerHTML = editedEmpGender;
+      
+        // Hide the edit popup
+        document.getElementById('editPopup').style.display = 'none';
+      }
+      
+
+
+function onCancel(){
+    document.getElementById('editPopup').style.display = 'none';
 }
